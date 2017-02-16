981e4f0545a561929dd7f7062aafbbc96fb77faa
Jan Hrubeš <jan@hrubesovi.com>
2017-02-16 17:43:22 +0100

    EsLint settings, code reformat

---
+++ app/src/renderer/components/BrowserView.vue
@@ -0,0 +1,21 @@
+<style>
+
+</style>
+
+<template>
+	<div>
+		<img src="./BrowserView/assets/logo.big.png" alt="Logo" width="200">
+		<h1>Welcome to PhotoBooth</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		created () {
+			let self = this
+			setTimeout(function () {
+				self.$router.push('/landing-page')
+			}, 2000)
+		}
+	}
+</script>
--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -1,16 +1,21 @@
 export default [
-  {
-    path: '/',
-    name: 'landing-page',
-    component: require('components/LandingPageView')
-  },
-  {
-    path: '/editor/:photo_id',
-    name: 'editor',
-    component: require('components/EditorView')
-  },
-  {
-    path: '*',
-    redirect: '/'
-  }
+	{
+		path     : '/landing-page',
+		name     : 'landing-page',
+		component: require('components/LandingPageView')
+	},
+	{
+		path     : '/editor/:photo_id',
+		name     : 'editor',
+		component: require('components/EditorView')
+	},
+	{
+		path     : '/',
+		name     : 'browser',
+		component: require('components/BrowserView')
+	},
+	{
+		path    : '*',
+		redirect: '/'
+	}
 ]
